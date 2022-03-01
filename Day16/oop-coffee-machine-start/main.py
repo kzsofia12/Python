527a3eb6550d56b72325db1b6effb3e204bf091d
Korita Zsofia <korita.zsofi12@gmail.com>
2022-03-01 20:42:56 +0100

    day16 start

---
+++ Day16/oop-coffee-machine-start/main.py
@@ -0,0 +1,11 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffeeMaker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu_item = MenuItem()
+
+choose = input("What would you like? (espresso/latte/cappuccino/): ")
+
